96e69c482c0e2a0262812ed314e5b9637011281d
Volodymyr Bezuglyy <vladimirbezugliy@gmail.com>
2017-03-28 16:33:10 +0300

    Initial commit

---
+++ install.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var path = require('path')
+var extract = require('extract-zip')
+
+const distDir = path.join(__dirname, 'dist')
+const zipFile = path.join(__dirname, 'iojs-1.6.1.zip')
+extractFile(zipFile, distDir)
+
+function extractFile (zipPath, toDir) {
+   extract(zipPath, {dir: toDir}, function (err) {
+      if (err) throw err
+
+      const srcLibDir   = path.join(distDir,    'ia32')
+      const srcLibFile  = path.join(srcLibDir,  'iojs.lib')
+      const destLibDir  = path.join(distDir,    'Release')
+      const destLibFile = path.join(destLibDir, 'iojs.lib')
+
+      fs.mkdir(destLibDir, () => fs.createReadStream(srcLibFile).pipe(fs.createWriteStream(destLibFile)))
+   })
+}
